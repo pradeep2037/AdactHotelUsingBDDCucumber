a01c1f31097c2f201864515ea186a253908f86a4
pradeep2037 <pradeep2037@gmail.com>
2026-08-29 09:53:18 +0530

    updated

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -4,58 +4,102 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import utils.ConfigReader;
 
 public class DriverFactory {
 
-	public static WebDriver driver;
+    public static WebDriver driver;
 
-	public static void initializeDriver() {
+    public static void initializeDriver() {
 
-		String browser = ConfigReader.getProperty("browser");
+        String browser = ConfigReader.getProperty("browser");
 
-		try {
+        // First preference: value passed from command line/Jenkins
+        String headlessProperty = System.getProperty("headless");
 
-			switch (browser.toLowerCase()) {
-			case "chrome":
-				driver = new ChromeDriver();
-				break;
-			case "edge":
-				driver = new EdgeDriver();
-				break;
+        // If not passed from Jenkins, read from config.properties
+        boolean headless = headlessProperty != null
+                ? Boolean.parseBoolean(headlessProperty)
+                : Boolean.parseBoolean(ConfigReader.getProperty("headless"));
 
-			default:
-				throw new RuntimeException("Unsupported browser: " + browser);
-			}
+        try {
 
-			driver.manage().window().maximize();
-			int pageLoadTimeOut = Integer.parseInt(ConfigReader.getProperty("pageLoadTimeout"));
+            switch (browser.toLowerCase()) {
 
-			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(pageLoadTimeOut));
+            case "chrome":
 
-		} catch (Exception e) {
-			throw new RuntimeException("Unable to initialize WebDriver for browser: " + browser, e);
-		}
+                ChromeOptions chromeOptions = new ChromeOptions();
 
-	}
+                if (headless) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                }
 
-	public static WebDriver getDriver() {
+                driver = new ChromeDriver(chromeOptions);
+                break;
 
-		if (driver == null) {
-			throw new RuntimeException("WebDriver is not initialized");
-		}
+            case "edge":
 
-		return driver;
-	}
+                EdgeOptions edgeOptions = new EdgeOptions();
 
-	public static void quitDriver() {
-		if (driver != null) {
-			driver.quit();
-			driver = null;
-		}
-	}
+                if (headless) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--window-size=1920,1080");
+                }
 
+                driver = new EdgeDriver(edgeOptions);
+                break;
+
+            default:
+                throw new RuntimeException("Unsupported browser: " + browser);
+            }
+
+            if (!headless) {
+                driver.manage().window().maximize();
+            }
+
+            int pageLoadTimeOut = Integer.parseInt(
+                    ConfigReader.getProperty("pageLoadTimeout")
+            );
+
+            driver.manage()
+                  .timeouts()
+                  .pageLoadTimeout(Duration.ofSeconds(pageLoadTimeOut));
+
+            System.out.println("=================================");
+            System.out.println("Browser       : " + browser);
+            System.out.println("Headless Mode : " + headless);
+            System.out.println("OS            : " + System.getProperty("os.name"));
+            System.out.println("Java Version  : " + System.getProperty("java.version"));
+            System.out.println("=================================");
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(
+                    "Unable to initialize WebDriver for browser: " + browser,
+                    e
+            );
+        }
+    }
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+            throw new RuntimeException("WebDriver is not initialized");
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
